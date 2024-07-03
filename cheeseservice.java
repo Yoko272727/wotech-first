d9eb5f9b1960684494eff96bb0f60242332672dd
Yoko272727 <148010448+Yoko272727@users.noreply.github.com>
2024-07-03 22:00:26 +0900

    Create cheeseservice.java

---
+++ cheeseservice.java
@@ -0,0 +1,11 @@
+public class CheeseService {
+    private CheeseShop cheeseShop;
+
+    public CheeseService() {
+        cheeseShop = new CheeseShop();
+    }
+
+    public CheeseShop getCheeseShop() {
+        return cheeseShop;
+    }
+}
